62f8db0e9726b892323d19015cd599b61f7c4391
tobiasbuhmann <tobias.buhmann@outlook.at>
2023-06-26 19:00:06 +0200

    Linting

--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
-const express = require("express")
+const express = require('express')
 const app = express()
 
 let notes = [
   {
     id: 1,
-    content: "HTML is easy",
-    date: "2022-01-10T17:30:31.098Z",
+    content: 'HTML is easy',
+    date: '2022-01-10T17:30:31.098Z',
     important: true
   },
   {
     id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2022-01-10T18:39:34.091Z",
+    content: 'Browser can execute only Javascript',
+    date: '2022-01-10T18:39:34.091Z',
     important: false
   },
   {
     id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2022-01-10T19:20:14.298Z",
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    date: '2022-01-10T19:20:14.298Z',
     important: true
   }
 ]
 
 app.use(express.json())
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello World!</h1>")
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
 })
 
 const generateId = () => {
@@ -35,12 +35,12 @@ const generateId = () => {
   return maxId + 1
 }
 
-app.post("/api/notes", (request, response) => {
+app.post('/api/notes', (request, response) => {
   const body = request.body
 
   if (!body.content) {
     return response.status(400).json({
-      error: "content missing"
+      error: 'content missing'
     })
   }
 
@@ -56,18 +56,18 @@ app.post("/api/notes", (request, response) => {
   response.json(note)
 })
 
-app.get("/api/notes", (req, res) => {
+app.get('/api/notes', (req, res) => {
   res.json(notes)
 })
 
-app.delete("/api/notes/:id", (request, response) => {
+app.delete('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id)
   notes = notes.filter(note => note.id !== id)
 
   response.status(204).end()
 })
 
-app.get("/api/notes/:id", (request, response) => {
+app.get('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id)
   const note = notes.find(note => note.id === id)
 
